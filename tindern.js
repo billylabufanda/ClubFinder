e72295db0cf5041e9fd59345c364acce195448af
billylabufanda <billylabufanda@users.noreply.github.com>
2016-09-08 12:27:03 -0700

    Update tindern.js

--- tindern.js
+++ tindern.js
@@ -223,6 +223,9 @@ var returnSavedInternshipPageBackToRegularInternshipsBoolean = true
     
 function onSuccess(googleUser) {
   console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 }
 function onFailure(error) {
   console.log(error);
@@ -240,10 +243,4 @@ function goGoGoogle() {
   console.log('did it go?');
 }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
+
