2ede3b12f1d2a3c36c98763c6846bc380db73d1d
billylabufanda <billylabufanda@users.noreply.github.com>
2016-09-08 12:24:20 -0700

    Update tindern.js

--- tindern.js
+++ tindern.js
@@ -239,3 +239,11 @@ function goGoGoogle() {
   });
   console.log('did it go?');
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
